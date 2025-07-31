8de0d4474df6da102027f67a28650eb91c7b246d
Aden <adenzhu01@gmail.com>
2025-07-31 16:11:10 -0700

    added walls and bugfix

--- Assets/Scripts/WalkScript.cs
+++ Assets/Scripts/WalkScript.cs
@@ -7,7 +7,6 @@ public class WalkScript : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private BoxCollider2D playerCollider;
     [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private LayerMask wallLayer;
     [SerializeField] private float speed;
     private bool goingLeft;
 
@@ -37,24 +36,24 @@ public class WalkScript : MonoBehaviour
     {   
         if (goingLeft)
         {
-            RaycastHit2D wallCheck = Physics2D.BoxCast(playerCollider.bounds.center, playerCollider.bounds.size, 0, Vector2.left, 0.1f, wallLayer);
+            RaycastHit2D wallCheck = Physics2D.BoxCast(playerCollider.bounds.center, playerCollider.bounds.size, 0, Vector2.left, 0.1f, groundLayer);
 
-            rb.Velocity = speed * Vector2.left
+            rb.velocity = speed * Vector2.left;
 
             if (wallCheck.collider != null)
             {
-                goingLeft = false
+                goingLeft = false;
             }
         } 
         else
         {
-            RaycastHit2D wallCheck = Physics2D.BoxCast(playerCollider.bounds.center, playerCollider.bounds.size, 0, Vector2.right, 0.1f, wallLayer);
+            RaycastHit2D wallCheck = Physics2D.BoxCast(playerCollider.bounds.center, playerCollider.bounds.size, 0, Vector2.right, 0.1f, groundLayer);
 
-            rb.Velocity = speed * Vector2.right
+            rb.velocity = speed * Vector2.right;
 
             if (wallCheck.collider != null)
             {
-                goingLeft = true
+                goingLeft = true;
             }
         }
         
